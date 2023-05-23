f7ace3ce0f4505ec6a4d3f4b08f512e9b9b7ffa5
abdulmalik-devs <malexmazzy@gmail.com>
2023-05-23 01:00:43 +0100

    changes

--- client-admin.sh
+++ client-admin.sh
@@ -6,9 +6,6 @@ echo "*****************************************************************"
 sudo apt-get update
 sudo apt-get install git -y
 
-echo "---------------------------------------------"
-echo "--INSTALL NVM TO USE A SPECIFIC NODE VERSION--"
-echo "----------------------------------------------"
 sudo apt-get install curl
 sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -37,69 +34,42 @@ echo "******************************************"
 echo "Performing the following actions for Client"
 echo "******************************************"
 
-sudo cd
-sudo rm -rf full-ecommerce || true
 sudo git clone https://github.com/abdulmalik-devs/mern-ecommerce-project.git
-sudo cd mern-ecommerce-project/
-sudo cd web_panel
+cd mern-ecommerce-project/web_panel
 sudo rm -rf build
 sudo npm install
 sudo npm run build
-sudo pm2 delete react-build || true
+pm2 serve build/ 3000 --name "client-server"
 
-# Create a new Nginx site configuration file
 sudo tee /etc/nginx/sites-available/myapp.conf > /dev/null <<EOF
 server {
     listen 80 default_server;
-    server_name _;  # Replace with your actual domain or IP address
+    server_name _;  
 
     location / {
         proxy_pass http://localhost:3000;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
     }
 
     location /admin {
         proxy_pass http://localhost:3001;
-        proxy_http_version 1.1;
-        proxy_set_header Upgrade \$http_upgrade;
-        proxy_set_header Connection 'upgrade';
-        proxy_set_header Host \$host;
-        proxy_cache_bypass \$http_upgrade;
     }
 }
 EOF
 
-# Remove the default Nginx site configuration file
 sudo rm -f /etc/nginx/sites-enabled/default
-
-# Create a symbolic link to enable the new site configuration
 sudo ln -s /etc/nginx/sites-available/myapp.conf /etc/nginx/sites-enabled/
-
-# Test the Nginx configuration for any syntax errors
 sudo nginx -t
-
-# Restart Nginx to apply the new configuration
 sudo systemctl restart nginx
-
-pm2 serve build/ 3000 -f --name "client-server" --spa
-
 
 echo "******************************************"
 echo "Performing the following actions for Admin"
 echo "******************************************"
 
-sudo cd /home/ubuntu
-sudo cd mern-ecommerce-project/
-sudo cd web_admin
-sudo rm -rf build
+cd /home/ubuntu
+cd mern-ecommerce-project/web_admin
 sudo npm install
 sudo npm run build
-sudo pm2 delete react-admin || true
-sudo pm2 serve build/ 3001 -f --name "admin-server" --spa
+sudo pm2 serve build/ 3001 --name "admin-server"
 
 
 echo "******************************************"
--- server.sh
+++ server.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-sudo ssh -i ~/.ssh/key.pem ubuntu@INSTANCE_ID_AZ1
-
-echo "*****************************************************************"
-echo "Performing the following actions on Server Instance in AZ1"
-echo "*****************************************************************"
+echo "****************************************************"
+echo "Performing the following actions on Server Instance"
+echo "***************************************************"
 
 sudo apt-get update
 sudo apt-get install git -y
 
-echo "--INSTALL NVM TO USE A SPECIFIC NODE VERSION--"
 sudo apt-get install curl
 sudo curl https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" 
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
 nvm install 18
+sudo apt-get install npm -y
+nvm alias default 18
+npm -v
+node -v
 
 echo "-----PM2------"
 sudo npm install -g pm2
@@ -30,22 +32,30 @@ sudo ufw allow 'Nginx Full'
 sudo ufw --force enable 
 
 echo "******************************************"
-echo "Performing the following actions on Server"
+echo "---------Clonning from Repo---------------"
 echo "******************************************"
 
-cd /home/ubuntu
-sudo rm -rf server || true
-sudo git clone <git url>
-cd full-ecommerce/
-cd web_services
+sudo git clone https://github.com/abdulmalik-devs/mern-ecommerce-project.git
+cd mern-ecommerce-project/web_services
 sudo npm install
-sudo pm2 kill
-sudo kill $(lsof -nt -i4TCP:5002)
 sudo pm2 start index.js --name "backend-server"
-sudo rm -rf /etc/nginx/sites-available/default
-sudo cp server-config /etc/nginx/sites-available/ -r
-sudo systemctl kill nginx || true
-sudo systemctl start nginx
+
+sudo tee /etc/nginx/sites-available/myapp.conf > /dev/null <<EOF
+server {
+    listen 80 default_server;
+    server_name _;  
+
+    location / {
+        proxy_pass http://localhost:5002/api/;
+    }
+}
+EOF
+
+sudo rm -rf /etc/nginx/sites-enabled/default
+sudo ln -s /etc/nginx/sites-available/myapp.conf /etc/nginx/sites-enabled
+sudo nginx -t
+sudo systemctl restart nginx
+
 
 
 
