3fd16bc15b99d3180ab11fb3d22bcc133e79b4ab
abdulmalik-devs <malexmazzy@gmail.com>
2023-07-19 20:27:23 +0100

    changes made to yml file

--- modules/ec2_private/main.tf
+++ modules/ec2_private/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "server_instance" {
   ami           = "ami-007855ac798b5175e"
   instance_type = "t2.micro"
-  key_name      = "web"
+  key_name      = "word"
   subnet_id     = var.subnet_id
   vpc_security_group_ids = "${var.security_group_id}"
 
